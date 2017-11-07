1d32f3f704ee422411ce3bd67e270504b3a3434a
sofa_king <apalm112@gmail.com>
2017-11-06 16:15:21 -0800

    new files.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# test-calculator
+# calculator.js
+
+Simple calculation functions
+
+
+## Install Dependancies
+
+```bash
+$ npm install
+```
+
+## Test Project
+
+```bash
+$ npm test
+```
---
+++ test/test_calculator_multiplication.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {assert} = require('chai');
+
+describe('multiply()', () => {
+    it('should multiply positive numbers together');
+
+    it('should multiply positive and negative numbers together');
+
+    it('should multiply negative numbers together');
+});
